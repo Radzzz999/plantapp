e6fdf74c03fc5a4af08bfd11906733d19e51731b
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-05-23 04:17:30 +0700

    add home_screen

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plantapp/components/my_bottom_nav_bar.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/body.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            elevation: 10,
+            pinned: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: kPrimaryColor,
+            expandedHeight: 10,
+            flexibleSpace: LayoutBuilder(
+              builder: (context, constraints) {
+                // Ubah opacity berdasarkan scroll
+                final double top = constraints.biggest.height;
+                final double opacity = (1 - (top - kToolbarHeight) / 50).clamp(0.0, 1.0);
+
+                return Container(
+                  
+                  child: AppBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    leading: IconButton(
+                      icon: SvgPicture.asset("assets/icons/menu.svg"),
+                      onPressed: () {},
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // Body
+          SliverToBoxAdapter(child: Body()),
+        ],
+      ),
+      bottomNavigationBar: SafeArea(
+      child: const MyBottomNavBar(currentIndex: 0),)
+    );
+  }
+}
