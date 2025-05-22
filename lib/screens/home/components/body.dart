2e43cf894bee7feb2da6ebe266bcd52264fcb234
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-05-23 04:18:51 +0700

    add body dart

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+
+import 'featurred_plants.dart';
+import 'header_with_seachbox.dart';
+import 'recomend_plants.dart';
+import 'title_with_more_bbtn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // It will provie us total height  and width of our screen
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+           HeaderWithProfileInfo(
+            size: size,
+            name: 'Agung',
+            address: 'jln.werkudara ,yogyakarta',
+          ),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
